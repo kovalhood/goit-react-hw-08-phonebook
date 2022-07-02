00d5e9357509b834bd6fa83a393cad710ed9f9f9
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-02 13:05:32 +0300

    Created_Dropdown_For_Logout

--- src/components/AppBar/UserMenu/UserMenu.jsx
+++ src/components/AppBar/UserMenu/UserMenu.jsx
@@ -1,19 +1,38 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { authSelectors, authOperations } from 'redux/auth';
+import { useState } from 'react';
 import sprite from '../../../images/icons.svg';
 import s from './UserMenu.module.css';
 
 export default function UserMenu() {
+    const [dropdown, setDropdown] = useState(false);
+
     const dispatch = useDispatch();
     const name = useSelector(authSelectors.getUserName);
+    const email = useSelector(authSelectors.getUserEmail);
+
+    const handleDropdown = () => {
+        setDropdown(prevState => !prevState);
+    }
 
     return <div className={s.user_menu}>
-        <svg className={s.icon} aria-label="user">
-            <use href={`${sprite}#user1`}></use>
-        </svg>
-        <span className={s.user_name}> {name} </span>
-        <button type='button' className={s.button} onClick={() => dispatch(authOperations.logOut())}>
-            Log Out
-        </button>
+        <div className={s.user_info} onClick={handleDropdown}>
+            <span className={s.user_name}> {name} </span>
+            <svg className={s.icon} aria-label="user">
+                <use href={`${sprite}#user`}></use>
+            </svg>
+        </div>
+        
+        {dropdown &&
+            <div className={s.test}>
+                <div className={s.dropdown}>
+                    <span className={s.user_mail}> {email} </span>
+
+                    <button type='button' className={s.button} onClick={() => dispatch(authOperations.logOut())}>
+                        Log Out
+                    </button>
+                </div>
+            </div>
+        }
     </div>
 }
